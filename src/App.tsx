b0b7f4123c5705015a8b37892c8a008460f70bca
jay10111 <166088964+jay10111@users.noreply.github.com>
2025-02-15 12:09:04 +0530

    port

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+
 
 import './App.css'
 import Nav from './Nav'
 import Hero from './Hero'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
